9a5147e4ea36767e0d9b23696d5b323f3e6f81c3
Gerald Nash <g.nash.dev@gmail.com>
2016-12-23 20:37:01 -0800

    Add zeroes and tangent endpoints

--- README.md
+++ README.md
@@ -35,10 +35,12 @@ Note: `:operation` is the math operation that you want to perform. `:expression`
 ### View available endpoints:
 | Operation |    API Endpoint   |      Response     |
 |:---------:|:-----------------:|:-----------------:|
-| Simplify  | /simplify/2^2+2(2) | 8                 |
+| Simplify  | /simplify/2^2+2(2)| 8                 |
 | Factor    | /factor/x^2 + 2x  | x (x + 2)         |
 | Derive    | /derive/x^2+2x    | 2 x + 2           |
 | Integrate | /integrate/x^2+2x | 1/3 x^3 + x^2 + C |
+| Find 0's  | /zeroes/x^2+2x    | [0, -2]           |
+| Find Tangent| /tangent/2lx^3  | 12 x + -16        | Note: the 'l' is a bar, not an 'L'
 
 
 
--- app.js
+++ app.js
@@ -3,23 +3,59 @@ var algebrite = require('algebrite');
 var path      = require('path');
 var app       = express();
 
+var zeroes = function(oExpression){
+  var _zeroes = [];
+  // x(x+1)(x+2) -> [ x, x+1), x+2) ]
+  expression = oExpression.split(' ').join('').split('(');
+  expression.forEach(function(item){
+    var xIdx = item.indexOf('x');
+    // ax+b=0, so x=-b/a
+    var a = item.substring(xIdx-1, xIdx);
+    var b = item.substring(xIdx+1, xIdx+3);
+
+    if(a === '')
+      a = 1;
+    if(b === '')
+      b = 0;
+    _zeroes.push(-b/a);
+  });
+  if(oExpression.charAt(0) === '(')
+    _zeroes = _zeroes.slice(1);
+  return JSON.stringify(_zeroes);
+};
+
+var tangent = function(rawData){
+  // 2|x^3 -> Find tangent line of x^3 at x = 2
+  var data = rawData.split('|');
+  var x = data[0];
+  var expression = data[1];
+
+  // f(x) - xf'(x) = b
+  var fx = parseInt(algebrite.run(expression.split('x').join(x)));
+  var fp = algebrite.derivative(expression).toString();
+  var fpx = algebrite.run(fp.split('x').join(x));
+  var b = fx - parseInt(x) * parseInt(fpx);
+
+  return fpx + ' x + ' + b;
+};
+
 var operationMap = {
   simplify: algebrite.run,
   factor: function(expression){
     return algebrite.factor(expression).toString();
   },
-  /*expand: function(expression){
-    return algebrite.expand(expression);
+  zeroes: function(expression){
+    return zeroes(algebrite.factor(expression).toString());
   },
-  contract: function(expression){
-    return algebrite.contract(expression).toString();
-  },*/
   integrate: function(expression){
     return algebrite.integral(expression).toString() + ' + C';
   },
   derive: function(expression){
     return algebrite.derivative(expression).toString();
   },
+  tangent: function(data){
+    return tangent(data);
+  }
 };
 
 // Send index.html when root route is accessed (<- homophones ftw!)
@@ -29,14 +65,21 @@ app.get('/', function(req, res) {
 
 // A dynamic API endpoint where we do our busy work
 app.get('/:operation/:data', function(req, res){
-  var computation = operationMap[req.params.operation](req.params.data);
-  res.send({
-    operation: req.params.operation,
-    expression: req.params.data,
-    result: computation
-  });
-});
+  var f = operationMap[req.params.operation];
 
+  if(f === undefined){
+    res.send({
+      error: 'Unknown operation'
+    });
+  } else {
+    var computation = f(req.params.data);
+    res.send({
+      operation: req.params.operation,
+      expression: req.params.data,
+      result: computation
+    });
+  }
+});
 app.listen(3000, function(){
   console.log('We\'re up at 3000!');
 });
